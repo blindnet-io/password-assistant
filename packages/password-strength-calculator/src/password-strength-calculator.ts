38660fa4462a8c63f6ea866eebe38adca649801a
Justin Boileau <jboileau@ualberta.ca>
2022-06-29 18:12:55 +0200

    Updated password-strength-calculator.ts

--- packages/password-strength-calculator/src/password-strength-calculator.ts
+++ packages/password-strength-calculator/src/password-strength-calculator.ts
@@ -1,29 +1,4 @@
-export class PasswordStrengthCalculator {
-
-    constructor(
-        readonly components: ((password: string) => EvalResult)[],
-        public requiredScore: number
-    ) {}
-
-    /**
-     * Execute each component and save the EvalResults.
-     * @param password
-     */
-    run(password: string) {
-        return this.components.map(evaluator => evaluator(password))
-    }
-
-    /**
-     * Add another evaluator function to a calculator instance.
-     * @param evaluator
-     */
-    addEvaluator(evaluator: (password: string) => EvalResult) {
-        this.components.push(evaluator)
-    }
-
-}
-
-export interface EvalResult {
+export interface EvaluatorResult {
     pass: boolean,
     score: number,
     recommendations: {
@@ -35,3 +10,57 @@ export interface PasswordRecommendation {
     text: string
     weight: number
 }
+
+// export type Evaluator = (password: string) => EvaluatorResult
+//
+// export type WeightedEvaluator = [number, Evaluator]
+
+export interface Evaluator {
+    func: (password: string) => EvaluatorResult
+}
+
+export interface WeightedEvaluator extends Evaluator {
+    weight: number
+}
+
+export class PasswordStrengthCalculator {
+
+    constructor(
+        readonly components: Evaluator[] | WeightedEvaluator[],
+        public requiredScore: number
+    ) {}
+
+    /**
+     * Execute each component and save the EvalResults.
+     * @param password
+     */
+    run(password: string) {
+
+        // Execute each evaluator
+        const result = this.components.map(evaluator => evaluator(password))
+
+        // Calculate total score
+        // if (this.components instanceof WeightedEvaluator)
+
+        // Combine (and sort?) recomendations
+    }
+
+    /**
+     * Add another evaluator function to a calculator instance.
+     * @param {Evaluator} evaluator
+     */
+    // addEvaluator(evaluator: Evaluator) {
+    //     this.components.push(evaluator)
+    // }
+
+}
+
+// export class PasswordEvaluator {
+//
+//     constructor(
+//         public weight: number = 1,
+//         private evaluator: Evaluator,
+//     ) {
+//     }
+//
+// }
